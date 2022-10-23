7aee75dcf9708221a7997fecf7fb54d02d3f72d6
lert <tplermit@gmail.com>
2022-10-23 18:44:00 -0400

    models

---
+++ models/Entry.ts
@@ -0,0 +1,21 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { Entry } from "../interfaces";
+
+interface IEntry extends Entry { }
+
+
+const entrySchema = new Schema({
+    description: { type: String, require: true },
+    createAt: { type: Number },
+    status: {
+        type: String,
+        enum: {
+            values: ['pennding', 'in-progress', 'finished'],
+            message: '{VALUE} no allowed!'
+        }
+    }
+})
+
+const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model('Entry', entrySchema)
+
+export default EntryModel
